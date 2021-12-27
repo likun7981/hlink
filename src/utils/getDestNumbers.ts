9e8c896834466b000b076ace8a3ab1255ac3ab6b
likun7981 <likun7981@gmail.com>
2021-12-28 03:40:48 +0800

    fix: some problem

--- src/utils/getDestNumbers.ts
+++ src/utils/getDestNumbers.ts
@@ -1,18 +1,22 @@
 import execa from 'execa'
 import path from 'path'
 import getFileAndNumber from './getFileAndNumber'
+import fs from 'fs-extra'
 
 function getDestNumbers(dest: string) {
   let result: string[] = []
-  execa
-    .sync('ls', ['-iRF', dest])
-    .stdout.split('\n')
-    .forEach(file => {
-      if (Boolean(file) && !file.endsWith(path.sep) && !file.endsWith(':')) {
-        const [number] = getFileAndNumber(file)
-        result.push(number)
-      }
-    })
+
+  if (fs.existsSync(dest)) {
+    execa
+      .sync('ls', ['-iRF', dest])
+      .stdout.split('\n')
+      .forEach(file => {
+        if (Boolean(file) && !file.endsWith(path.sep) && !file.endsWith(':')) {
+          const [number] = getFileAndNumber(file)
+          result.push(number)
+        }
+      })
+  }
   return {
     result
   }
