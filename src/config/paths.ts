65905c8697370bc351f7616da48b16715d7151ed
likun7981 <likun7981@gmail.com>
2021-12-28 23:29:23 +0800

    fix: new cahce name

--- src/config/paths.ts
+++ src/config/paths.ts
@@ -6,8 +6,8 @@ export const hlinkHomeDir = path.join(os.homedir(), '.hlink')
 
 export const configName = 'hlink.config.js'
 export const configPath = path.join(os.homedir(), configName)
-export const cachePath = path.join(hlinkHomeDir, 'cache.json')
+export const cachePath = path.join(hlinkHomeDir, 'cache-array.json')
 
 export const deleteConfig = new Config<Record<string, string[]>>('cacheForDelete.json', {})
 
-export const cacheConfig = new Config<Array<string>>('cache.json', [])
+export const cacheConfig = new Config<Array<string>>('cache-array.json', [])
