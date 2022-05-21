11488060d25573c4487397c7141e0d69c67c05a2
likun7981 <likun7981@gmail.com>
2022-05-22 03:34:08 +0800

    fix: postinstall

--- src/doctor-dev.ts
+++ src/doctor-dev.ts
@@ -1,3 +1,4 @@
+#!/usr/bin/env node
 process.env.NODE_ENV = 'development'
 
 import('./doctor.js')
--- src/doctor.ts
+++ src/doctor.ts
@@ -1,3 +1,4 @@
+#!/usr/bin/env node
 import chalk from 'chalk'
 import { execaSync } from 'execa'
 import path from 'path'
