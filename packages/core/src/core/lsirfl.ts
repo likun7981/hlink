4d72f3174be8f372ee49946311bce16d91529809
likun7981 <likun7981@gmail.com>
2022-07-06 02:27:50 +0800

    fix: dist dir not exist

--- packages/core/src/core/lsirfl.ts
+++ packages/core/src/core/lsirfl.ts
@@ -1,6 +1,8 @@
 import { execa } from 'execa'
+import fs from 'fs-extra'
 
 async function lsirf(path: string, ignoreError = false): Promise<string> {
+  await fs.ensureDir(path)
   try {
     return (await execa('ls', ['-iRFL', path])).stdout
   } catch (e) {
