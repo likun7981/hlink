8130394888e28d00db7cec9270f778af99d4f490
likun7981 <likun7981@gmail.com>
2022-08-03 00:56:12 +0800

    fix(core): ext globs should use **/*.ext otherwise **.ext

--- packages/core/src/utils/getGlobs.ts
+++ packages/core/src/utils/getGlobs.ts
@@ -16,7 +16,7 @@ const getGlobs = (
   let { globs, exts } = options || {}
   globs = globs || []
   if (exts) {
-    globs = globs.concat(exts.map((ext) => `**.${ext}`))
+    globs = globs.concat(exts.map((ext) => `**/*.${ext}`))
   }
   if (!globs.length) {
     globs = globs.concat(defaultGlobs)
--- packages/core/src/utils/supported.ts
+++ packages/core/src/utils/supported.ts
@@ -3,6 +3,7 @@ import micromatch from 'micromatch'
 function supported(path: string, include: string[], exclude: string[]) {
   return micromatch.isMatch(path.toLowerCase(), include, {
     ignore: exclude,
+    nocase: true,
   })
 }
 
